2f144e2d4dc1faf09ea742052bb2b3967db407a9
Shoaib Shakeel <shoaibshakeel381@yahoo.com>
2018-05-07 15:42:55 +0500

    Added Data Access Layer

---
+++ Data Access Layer/Rebtel.DataEntities/Subscription.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rebtel.DataEntities
+{
+    public class Subscription
+    {
+    }
+}
